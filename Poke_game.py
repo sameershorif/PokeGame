e563b121051fe259596314df8acb302a45a2e29f
nettie168 <antoinette.alevropoulos@gmail.com>
2025-11-17 16:39:10 +0000

    add height cpu

--- Poke_game.py
+++ Poke_game.py
@@ -52,3 +52,14 @@ cpu_name = cpu_pokemon_data['name']
 
 print(cpu_pokemon_data['name'])
 print(f"CPU Pokémon: {cpu_name}")
+
+#### Battle
+
+# to get ability
+cpu_abilities = cpu_pokemon_data['abilities'][0]
+cpu_ability = cpu_abilities['ability']
+
+# to format height and weight properly
+cpu_height = int(cpu_pokemon_data['height'])
+
+print(f'CPU Pokemon Height: {cpu_height}')
