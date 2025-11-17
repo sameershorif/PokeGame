ee74a4e30fb7cf30332674b56c13b630642376ef
nettie168 <antoinette.alevropoulos@gmail.com>
2025-11-17 16:21:00 +0000

    random pokemon for CPU

--- Poke_game.py
+++ Poke_game.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import random
 
 # Get the list of pokemon from the API
 url = 'https://pokeapi.co/api/v2/pokemon/'
@@ -36,3 +37,14 @@ print('Name: {}'.format(pokemon_data['name']))
 print('Weight: {}'.format(weight_formatted) + "(kgs)")
 print('Height: {}'.format(height_formatted) + "(m)")
 print('Ability: {}'.format(ability['name']))
+
+######### Random Pokemon assigned to CPU
+
+random_id = random.randint(0, len(pokemon_list) - 1)
+
+random_poke_url = f'https://pokeapi.co/api/v2/pokemon/{random_id}/'
+
+response = requests.get(random_poke_url)
+pokemon_data = json.loads(response.text)
+
+print(pokemon_data['name'])
